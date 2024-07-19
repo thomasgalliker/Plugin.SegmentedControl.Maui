f94ea6328563ce4cdad93ec6b55931d140ef4a88
Thomas Galliker <>
2024-07-19 09:04:47 +0200

    Remove appearing event subscription

--- Samples/SegmentedControlDemoApp/ViewModels/MainViewModel.cs
+++ Samples/SegmentedControlDemoApp/ViewModels/MainViewModel.cs
@@ -1,57 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.Extensions.Logging;
 using SegmentedControlDemoApp.Services;
 
 namespace SegmentedControlDemoApp.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
-        private readonly ILogger logger;
         private readonly INavigationService navigationService;
-        private readonly IDialogService dialogService;
 
-        private IAsyncRelayCommand appearingCommand;
-        private bool isInitialized;
         private IAsyncRelayCommand<string> navigateToPageCommand;
-        private int selectedSegment;
 
         public MainViewModel(
-            ILogger<MainViewModel> logger,
-            INavigationService navigationService,
-            IDialogService dialogService)
+            INavigationService navigationService)
         {
-            this.logger = logger;
             this.navigationService = navigationService;
-            this.dialogService = dialogService;
-        }
-
-
-        public IAsyncRelayCommand AppearingCommand
-        {
-            get => this.appearingCommand ??= new AsyncRelayCommand(this.OnAppearingAsync);
-        }
-
-        private async Task OnAppearingAsync()
-        {
-            if (!this.isInitialized)
-            {
-                await this.InitializeAsync();
-                this.isInitialized = true;
-            }
-        }
-
-        private async Task InitializeAsync()
-        {
-            try
-            {
-                // TODO
-            }
-            catch (Exception ex)
-            {
-                this.logger.LogError(ex, "InitializeAsync failed with exception");
-                await this.dialogService.DisplayAlertAsync("Error", "Initialization failed", "OK");
-            }
         }
 
         public IAsyncRelayCommand<string> NavigateToPageCommand
