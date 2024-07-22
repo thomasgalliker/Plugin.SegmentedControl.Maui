7ae782572cf1c6aee4e65ec21eca82b6d9f5c0b3
Thomas Galliker <thomas@bluewin.ch>
2024-07-22 16:56:17 +0200

    Add unit tests

---
+++ Tests/Plugin.SegmentedControl.Maui.Tests/Utils/FontHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Plugin.SegmentedControl.Maui.Utils;
+using Xunit;
+
+namespace Plugin.SegmentedControl.Maui.Tests.Utils
+{
+    public class FontHelperTests
+    {
+        [Fact]
+        public void ShouldCreateFont()
+        {
+            // Arrange
+            var fontFamily = "monospace";
+            var fontSize = 22;
+            var fontAttributes = FontAttributes.Bold;
+
+            // Act
+            var font = FontHelper.CreateFont(fontFamily, fontSize, fontAttributes);
+
+            // Assert
+            font.Should().NotBeNull();
+            font.Size.Should().Be(fontSize);
+            font.Family.Should().Be(fontFamily);
+        }
+
+        // TODO: Add more unit tests here!
+    }
+}
