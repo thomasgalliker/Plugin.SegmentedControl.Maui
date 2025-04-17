ed15c8fe36b1cf59f8e110ebe1e42d96d85d5309
thomasgalliker <thomas@bluewin.ch>
2025-04-17 17:01:47 +0200

    Add shell sample app

---
+++ Samples/SegmentedControlTest/Tab2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+using SegmentedControlTest.Views;
+namespace SegmentedControlTest;
+
+public partial class Tab2Page : ContentPage
+{
+    public Tab2Page()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ Samples/SegmentedControlTest/Views/Modal1Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SegmentedControlTest.ViewModels;
+
+namespace SegmentedControlTest.Views;
+
+public partial class Modal1Page : ContentPage
+{
+	private readonly Modal1ViewModel _viewModel;
+	public Modal1Page()
+	{
+		InitializeComponent();
+		BindingContext = _viewModel = new Modal1ViewModel();
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		_viewModel.OnPropertyChanged(nameof(_viewModel.IsImageVisible));
+	}
+
+	private async void OnCloseModalClicked(object sender, EventArgs e)
+	{		
+		await Navigation.PopModalAsync();
+	}
+
+	private async void OnOpenModalClicked(object sender, EventArgs e)
+	{		
+		await Navigation.PushModalAsync(new NavigationPage(new Modal2Page()));
+	}
+}
+
